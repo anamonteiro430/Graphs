a332661ccef8bd6e708eaf3b2291e1372c9da574
kika <anamonteiro430@gmail.com>
2020-06-10 00:42:44 +0100

    mvp

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -34,54 +34,140 @@ class Graph:
         neighbors = self.vertices[vertex_id]
         return neighbors
 
-    def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+    def bft(self, starting_vertex): #✔
+        #use queue for BFT
+        queue = Queue()
+        visited = set()
+        #enqueue first vert
+        queue.enqueue(starting_vertex)
+        
+        #loop while queue is not empty:
+        while queue.size() != 0:
+            
+            #keep track of the current, when it enqueue it pops the value at index 0 and returns that value. That's the current
+            current = queue.dequeue()
+            print(current)  #visit it
+            if current not in visited: #if we haven't visited them yet
+                visited.add(current) #add it to visited, no need to check if it's already there because it's a set, unique values only        
 
-    def dft(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+                #check the neighbors of current
+                for n in self.get_neighbors(current):
+                    if n not in visited and n not in queue.queue:
+                        queue.enqueue(n) #enqueue it to our queue to check it
+        
+    def dft(self, starting_vertex): #✔
+        #use stack for DFT
+        stack = Stack()
+        #keep track of visited
+        visited = set()
 
-    def dft_recursive(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
+        #add to stack the starting vertex
+        
+        stack.push(starting_vertex)
 
-        This should be done using recursion.
-        """
-        pass  # TODO
+        while stack.size() != 0:
+            current = stack.pop()
+            print(current)
+            visited.add(current)
+
+            for n in self.get_neighbors(current):
+                if n not in visited and n not in stack.stack: #if we haven't visited them yet
+                    stack.push(n) #enqueue it to our queue to check it
+
+    def dft_recursive(self, starting_vertex, visited=None, stack=None):
+        #Initial Case
+        if visited is None: #keep track of visited
+            visited = set()    
+        
+        if stack is None:
+            stack = list()
+
+
+        stack.append(starting_vertex)
+
+        if len(stack) == 0:
+            return visited
+        
+        _next = stack.pop()
+        print(_next)
+        visited.add(_next)
+
+        for n in self.get_neighbors(_next):
+            if n not in visited and n not in stack:
+                self.dft_recursive(n, visited, stack)
 
     def bfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing the shortest path from
-        starting_vertex to destination_vertex in
-        breath-first order.
-        """
-        pass  # TODO
+             #use queue for BFT
+        queue = Queue()
+        visited = set()
+        #enqueue first vert
+        queue.enqueue([starting_vertex])
+        
+        #loop while queue is not empty:
+        while queue.size() != 0:
+            
+            #keep track of the current, when it enqueue it pops the value at index 0 and returns that value. That's the current
+            path = queue.dequeue()
+
+            if path[-1] not in visited:
+                if path[-1] == destination_vertex:
+                    return path
+                   
+                visited.add(path[-1]) #add it to visited, no need to check if it's already there because it's a set, unique values only        
+
+                #check the neighbors of current
+                for n in self.get_neighbors(path[-1]):
+                    new_path = list(path)
+                    new_path.append(n)
+                    queue.enqueue(new_path)
+                    
 
     def dfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
-        """
-        pass  # TODO
+        #use stack for DFT
+        stack = Stack()
+        #keep track of visited
+        visited = set()
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
+        #add to stack the starting vertex
+        
+        stack.push([starting_vertex])
 
-        This should be done using recursion.
-        """
-        pass  # TODO
+        while stack.size() != 0:
+            path = stack.pop()
+            print(path)
+
+            if path[-1] not in visited:
+                if path[-1] == destination_vertex:
+                    return path
+
+                visited.add(path[-1])
+
+                for n in self.get_neighbors(path[-1]):
+                    new_path = list(path)
+                    new_path.append(n)
+                    stack.push(new_path)
+
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
+        #Initial Case
+        if visited is None: #keep track of visited
+            visited = set()    
+        
+        if path is None:
+            path = list()
+
+
+        visited.add(starting_vertex)
+        path = path + [starting_vertex]
+
+        if starting_vertex == destination_vertex:
+            return path
+
+        for n in self.get_neighbors(starting_vertex):
+            if n not in visited:
+                n_path = self.dfs_recursive(n, destination_vertex, visited, path)
+                if n_path:
+                    return n_path
+        
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
